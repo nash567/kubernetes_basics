99bd0dca05df80b3e4d39c6f26d090a60fd5ded6
bipen2001 <bipen.c@gopherslab.com>
2022-11-27 22:56:45 +0530

    added fixes

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
-docker build -t bipen2001/multi-client:latest -t bipen2001/multi-client:${{github.sha}} -f ./client/Dockerfile ./client
-docker build -t bipen2001/multi-server:latest -t bipen2001/multi-server:${{github.sha}} -f ./server/Dockerfile ./server
-docker build -t bipen2001/multi-worker:latest -t bipen2001/multi-worker:${{github.sha}} -f ./worker/Dockerfile ./worker
+docker build -t bipen2001/multi-client:latest -t bipen2001/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t bipen2001/multi-server:latest -t bipen2001/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t bipen2001/multi-worker:latest -t bipen2001/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push bipen2001/multi-client:latest
-docker push bipen2001/multi-client:${{github.sha}}
+docker push bipen2001/multi-client:$SHA
 docker push bipen2001/multi-server:latest
-docker push bipen2001/multi-server:${{github.sha}}
+docker push bipen2001/multi-server:$SHA
 docker push bipen2001/multi-worker:latest
-docker push bipen2001/multi-worker:${{github.sha}}
+docker push bipen2001/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=bipen/multi-server:${{github.sha}}
-kubectl set image deployments/client-deployment server=bipen/multi-client:${{github.sha}}
-kubectl set image deployments/worker-deployment server=bipen/multi-worker:${{github.sha}}
+kubectl set image deployments/server-deployment server=bipen/multi-server:$SHA
+kubectl set image deployments/client-deployment server=bipen/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=bipen/multi-worker:$SHA
 
