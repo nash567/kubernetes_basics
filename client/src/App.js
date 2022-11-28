27c430fd72168302459b26b3b557f496c524af2b
bipen2001 <bipen.c@gopherslab.com>
2022-11-28 11:27:20 +0530

    updated to v2

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Fib calculator version kubernetes v1</h1>
+          <h1 className="App-title">Fib calculator version kubernetes v2</h1>
          
           <Link to="/">Home DIR</Link>
           <Link to="/otherpage">Other Page</Link>
